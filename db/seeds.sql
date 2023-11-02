f1ced97f13de3e1318ab07c0f4af731438569790
Aprecity <kathrynfisher3700@gmail.com>
2023-11-01 22:49:15 -0400

    updating seeds and testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,16 +5,16 @@ VALUES  (1, 'Sales'),
         (4, 'HR');
 
 INSERT INTO role (role_id, title, salary, department_id)
-VALUES  (1, 'Sales Agent'),
-        (2, 'Graphic Artist'),
-        (3, 'Admin'),
-        (4, 'Hiring Coordinator');
+VALUES  (1, 'Sales Agent', 80000, 1),
+        (2, 'Graphic Artist', 50000, 2),
+        (3, 'Admin', 60000, 3),
+        (4, 'Hiring Coordinator', 40000, 4);
 
 
-INSERT INTO employee (employee_id, first_name, last_name, manager_id)
-VALUES  (35, Theodore, Maxwell, 8),
-        (36, Althea, Jacobs, 8),
-        (37, Amira, Fisher, 9),
-        (38, Jax, Minesfield, 7),
-        (39, Andrew, Hythen, 7),
-        (40, Mackenna, Marx, 6);
+INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
+VALUES  (35, 'Theodore', 'Maxwell', 1, 8),
+        (36, 'Althea', 'Jacobs', 2, 9),
+        (37, 'Amira', 'Fisher', 1, 8),
+        (38, 'Jax', 'Minesfield', 4, 7),
+        (39, 'Andrew', 'Hythen', 3, 6),
+        (40, 'Mackenna', 'Marx', 3, 6);
