a3a4e47ca9675b2e4187434616b0329773223929
Aprecity <kathrynfisher3700@gmail.com>
2023-11-01 22:39:50 -0400

    updated schema with fill-in info

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_id, department_name)
+VALUES  (1, 'Sales'),
+        (2, 'Marketing'),
+        (3, 'Accounting'),
+        (4, 'HR');
+
+INSERT INTO role (role_id, title, salary, department_id)
+VALUES  (1, 'Sales Agent'),
+        (2, 'Graphic Artist'),
+        (3, 'Admin'),
+        (4, 'Hiring Coordinator');
+
+
+INSERT INTO employee (employee_id, first_name, last_name, manager_id)
+VALUES  (35, Theodore, Maxwell, 8),
+        (36, Althea, Jacobs, 8),
+        (37, Amira, Fisher, 9),
+        (38, Jax, Minesfield, 7),
+        (39, Andrew, Hythen, 7),
+        (40, Mackenna, Marx, 6);
